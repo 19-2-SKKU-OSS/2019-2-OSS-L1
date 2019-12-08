bf8e27d69507797db468c78ebb625d3112a65b74
sohee skkusosc <a41152271@gmail.com>
2019-12-09 02:19:13 +0900

    update editdistance.c

--- C/EditDistancealgorithm/editdistance.c
+++ C/EditDistancealgorithm/editdistance.c
@@ -31,7 +31,7 @@ int levenshtein(char* input1, char* input2){
                         printf("%d\t",dist[i][j]);
                         printf("\n");
         }
-        return dist[input1.length()][input2.length()];
+        return dist[strlen(input1)][input2.length()];
 }
 int main(){
         scanf("%s",input1);
