5472a95f8a81fe4bfd6e9945c851741ad524e797
sohee skkusosc <a41152271@gmail.com>
2019-12-09 02:23:49 +0900

    update editdistance.c

--- C/EditDistancealgorithm/editdistance.c
+++ C/EditDistancealgorithm/editdistance.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 
-const char* input1,input2;
+const char* input1 = malloc(sizeof(char)*CHAR_MAX);
+const char* input2 = malloc(sizeof(char)*CHAR_MAX);
 int dist[1001][1001];
 
 int min(int a, int b)
