a140a1ff732f76980323d59e25f75971eaf2505c
sohee skkusosc <a41152271@gmail.com>
2019-12-10 14:01:57 +0900

    update cycle.h

--- C/Cycle/cycle.c
+++ C/Cycle/cycle.c
@@ -73,7 +73,7 @@ int main()
     int n = sizeof(arr)/sizeof(arr[0]);
     cycleSort(arr,  n) ;
  
-    cout << "After sort : " <<endl;
+    printf("After sort :\n");
     for (int i =0; i<n; i++)
         cout << arr[i] << " ";
     return 0;
