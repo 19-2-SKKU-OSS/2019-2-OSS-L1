c2fe68ec546c73d99f595e808c85f699371773b4
tmznwnel skkusosc <sungjuyong00@gmail.com>
2019-12-13 02:03:38 +0900

    [ADD] Fibonacci

---
+++ C/Fibonacci/fibo2.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+
+int fibo(int num){
+    if(num == 0){
+        return 0;
+    }
+    else if(num == 1){
+        return 1;
+    }
+    else{
+        return fibo(num - 1) + fibo(num - 2);
+    }
+}
+
+int main(){
+    printf("%d", fibo(10));
+}
